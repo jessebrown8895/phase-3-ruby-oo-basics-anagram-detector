8900fb9006383d19c8d839ca3e05a10a7383b8ea
Jesse Brown <Brownjesse8895@yahoo.com>
2022-02-08 21:32:37 -0600

    commit tree 16ee713ba41deab5b45e89e52269a1dfb16b2b4b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+require 'pry' 
+class Anagram 
+    attr_accessor :anagram
+    def initialize(anagram)
+        @anagram = anagram
+    end 
+
+    def match 
+        puts anagram
+        binding.pry
+    end
+end
